02338b6adca6ec7a5971e1d0156806cc64c41193
Mdfaisal2002 <mohamedfaisal2311@gmail.com>
2026-08-10 23:51:14 +0530

    Fix API trailing slash

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
- const api = axios.create({
+const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-
-    headers : {
-        "Content-Type" : "application/json"
+    headers: {
+        "Content-Type": "application/json"
     }
-})
+});
+
+// Remove trailing slashes from API request URLs
+api.interceptors.request.use((config) => {
+    if (config.url) {
+        config.url = config.url.replace(/\/+$/, "");
+    }
+
+    return config;
+});
 
 export default api;
