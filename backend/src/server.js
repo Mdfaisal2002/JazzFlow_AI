9409c34a47915cc8bde0a4468dfb12c8ef096ae8
Mdfaisal2002 <mohamedfaisal2311@gmail.com>
2026-08-11 20:22:25 +0530

    Update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from './app.js'
-import connectDB from './config/db.js';
-connectDB();
+import dns from "dns";
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Running on : ${PORT}`);
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Running on : ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Server startup failed:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
